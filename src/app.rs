2960faca2f290375ef58922de3ffacfdcb4f1ef6
Ross Nomann <rossnomann@protonmail.com>
2020-03-16 10:16:35 +0400

    Add app

---
+++ src/app.rs
@@ -0,0 +1,72 @@
+use crate::{
+    config::{Config, ConfigError, WebhookUrl},
+    context::Context,
+    handler::{on_callback_query, on_message},
+};
+use carapax::{longpoll::LongPoll, webhook, Api, ApiError, Dispatcher};
+use hyper::Error as HyperError;
+use std::{env, fmt};
+
+pub async fn run() -> Result<(), Error> {
+    env_logger::init();
+    let Config {
+        api: api_config,
+        webhook_url,
+        chats,
+    } = match env::args().nth(1) {
+        Some(path) => Config::from_file(path).await?,
+        None => return Err(Error::ConfigPathMissing),
+    };
+    let api = Api::new(api_config)?;
+    let mut dispatcher = Dispatcher::new(Context {
+        api: api.clone(),
+        chats,
+    });
+    dispatcher.add_handler(on_message);
+    dispatcher.add_handler(on_callback_query);
+    match webhook_url {
+        Some(WebhookUrl { address, path }) => {
+            log::info!("Starting receiving updates via webhook: {}{}", address, path);
+            webhook::run_server(address, path, dispatcher)
+                .await
+                .map_err(Error::Webhook)?;
+        }
+        None => {
+            log::info!("Starting receiving updates via long polling");
+            LongPoll::new(api, dispatcher).run().await;
+        }
+    };
+    Ok(())
+}
+
+#[derive(Debug)]
+pub enum Error {
+    Api(ApiError),
+    Config(ConfigError),
+    ConfigPathMissing,
+    Webhook(HyperError),
+}
+
+impl From<ApiError> for Error {
+    fn from(err: ApiError) -> Self {
+        Error::Api(err)
+    }
+}
+
+impl From<ConfigError> for Error {
+    fn from(err: ConfigError) -> Self {
+        Error::Config(err)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
+        use self::Error::*;
+        match self {
+            Api(err) => write!(out, "{}", err),
+            Config(err) => write!(out, "{}", err),
+            ConfigPathMissing => write!(out, "You need to provide a path to config"),
+            Webhook(err) => write!(out, "Webhook error: {}", err),
+        }
+    }
+}
